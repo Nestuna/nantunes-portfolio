2c1e931edfc677bb89bbf7fdc9fc1cfa7dfeec22
Astuna <nicolasantunespereira@gmail.com>
2024-02-11 14:47:55 +0100

    edit some content

--- app/content.ts
+++ app/content.ts
@@ -25,7 +25,8 @@ export const projects: Project[] = [
     title: 'Portfolio ',
     projectContext: 'Projet personnel',
     description:
-      "Site vitrine pour exposer mes compétences mais aussi pour m\’entraîner sur React et Next. J'ai également pour projet d'intégrer une interface admin pour ajouter du contenu.",
+      "Site vitrine pour exposer mes compétences mais aussi pour me ré-initier sur les dernieres versions de **React** et **Next**. J'ai pu notament m'inspirer de l'*atomic design pattern* pour architecturer mon projet, tout en l'adaptant à mon style de code. \
+      \n \nJ'ai également pour projet d'intégrer une interface admin pour ajouter du contenu dynamiquement. J'ai déja préparé le terrain avec l'integration de *markdown*",
     stack: ['React', 'Next', 'Chakra UI'],
     repositories: [
       {
@@ -38,33 +39,39 @@ export const projects: Project[] = [
   {
     year: '2022-2023',
     title: "Refonte UI / UX d'une plateforme vidéo",
-    projectContext: 'Projet professionnel chez Ubicast',
+    projectContext: 'Projet professionnel chez Ubicast (alternance)',
     description:
-      "Design et developpement de la refonte UI / UX  intégrale de la navigation de Nudgis. \
-       \n Nudgis est une solution de plateforme video prive d\'e-learning a destination d\'institutions du superieur ou organisation publique ou privée. \n \
-       \n \nCela a été l\'occasion de m\'initier á Angular, mais aussi la construction d\'un projet de A á Z. J\'ai du parfois modifier l\'API, et apprendre sa logique. \
-       Egalement j\'ai pu monter en competence en TDD, en construisant des tests unitaires et end-to-end.",
+      "**Design et developpement** de la refonte UI / UX  intégrale de la navigation de *Nudgis*. \
+       \n *Nudgis* est une solution de plateforme vidéo prive d\'e-learning a destination d\'institutions du superieur ou organisation publique ou privée. \n \
+       \n \nCela a été l\'occasion de m\'initier à **Angular**, mais aussi à la **construction d\'un projet de A à Z** (y compris la démarche design UI / UX). J\'ai du parfois **modifier l\'API**, et apprendre sa logique. \
+       Egalement j\'ai pu monter en competence en TDD, en construisant des **tests unitaires** et **end-to-end**.",
 
-    stack: ['Angular', 'Material', 'Jest', 'Webdriver I/O', 'Django', 'Docker', 'TDD'],
+    stack: ['Angular', 'Material', 'Jest', 'Webdriver I/O', 'Django', 'Docker', 'TDD', 'Figma', 'Design UI / UX'],
     repositories: [],
     projectLink: '',
   },
   {
     year: '2020-2021',
     title: "Migration automatisée de contenu d'une plateforme vidéo (Nudgis)",
-    projectContext: 'Projet professionnel chez Ubicast',
+    projectContext: 'Projet professionnel chez Ubicast (alternance)',
     description:
-      "Migration automatisée depuis une platefome vidéo concurrente vers la plateforme Nudgis, afin que les clients aient tous les contenu presentable lors du passage á la solution d'Ubicast",
-    stack: ['Angular', 'Material', 'Jest', 'Webdriver I/O', 'Django', 'Docker'],
-    repositories: [],
+      "Migration automatisée depuis une platefome vidéo concurrente vers la plateforme *Nudgis*, afin que les clients aient tous le contenu presentable lors du passage à la solution d'Ubicast. \
+      \n \n Il a fallu **annalyser l'API de la plateforme concurrente**, et faire en sorte de **reproduire toute l'arboresence** sur la solution d'Ubicast en transférant les médias de facon massive, et de **transférer toutes les méta-données** possibles. \
+      Le projet est open-source et accessible publiquement.",
+    stack: ['Python', 'Scripting', 'Data fetching', 'Video computing', 'TDD'],
+    repositories: [{
+      title: 'Scripts app repository',
+      href: 'https://github.com/UbiCastTeam/mediasite-migration-scripts/tree/master',
+    }],
     projectLink: '',
   },
   {
     year: '2023',
     title: 'Cuistou',
-    projectContext: 'Projet universitaire autonome en groupe',
+    projectContext: 'Projet universitaire autonome en groupe - EFREI',
     description:
-      'Proposer des recettes en accord avec les fruits et légumes de saisons.',
+      'Proposer des recettes en accord avec les fruits et légumes de saisons. \n \
+      Possibilités d\avoir des recettes de saison mais aussi de voir ce qu\'il est possible de faire avec certains ingrédients',
     stack: ['React', 'Next', 'MUI', 'Express.js', 'MongoDB'],
     repositories: [
       {
@@ -77,9 +84,10 @@ export const projects: Project[] = [
   {
     year: '2020',
     title: 'Butterfly - Mobile App',
-    projectContext: 'Projet universitaire en duo',
+    projectContext: 'Projet universitaire en duo - CNAM',
     description:
-      "Messagerie éphémère et privée. Le but est pouvoir créer des conversations privée joignable seulement via un ID unique, et qui soit supprimée au bout d'un temps donnée. Le temps peut etre configurée",
+      "Messagerie éphémère et privée. \n \
+      Le but est pouvoir créer des conversations privées joignables seulement via un ID unique, et qu'elles soient au bout d'un temps donnée. Le temps peut etre configurée.",
     stack: ['React Native', 'Docker', 'Django Rest', 'MySQL'],
     repositories: [
       {
@@ -98,8 +106,9 @@ export const projects: Project[] = [
     title: '[ En projet ] JukeBoxify',
     projectContext: 'Projet personnel',
     description:
-      `Permettre à des personnes d’ajouter des morceaux à une liste de lecture Spotify maitre en cours.
-       Il suffirait d\avoir soit un lien, soit un QR Code. Le compte maitre peut configurer les modalites d\ajout`,
+      'Permettre à des personnes d’ajouter des morceaux à une liste de lecture Spotify maitre en cours. \
+       Pas besoin de plusieurs comptes spotify et d\'être sur le même réseau, juste un seul compte. \
+      \n Il suffirait d\'avoir soit un lien, soit un QR Code partagé. Le compte maitre peut configurer les modalites d\'ajout ou d\autres options',
     stack: ['React', 'Next', 'MUI', 'PWA', 'Express.js', 'Prisma', 'MongoDB'],
     repositories: [],
     projectLink: '',
@@ -114,10 +123,10 @@ export const experiences: Record<'pro' | 'training', Experience[]> = {
       organisation: 'Ubicast',
       // each item of array mark a line. So `,` is like a breakline
       description:
-      'Solutions d\’e-learning clé en main : Enregistreur optimisé  (Miris)  et Plateforme Vidéo (Nudgis). \n \
-      Missions :  \n \
-      -   Design et refonte UI / UX  intégrale de la navigation de Nudgis  \n \
-      -   Automatisation de la migration du contenu vidéo d\’une plateforme externe  vers Nudgis. \n \
+      'Solutions d\’e-learning clé en main : Enregistreur optimisé  (Miris)  et Plateforme Vidéo (Nudgis).  \
+      \n**Missions** :  \n \
+      \n-   Design et refonte UI / UX  intégrale de la navigation de Nudgis  \n \
+      \n-   Automatisation de la migration du contenu vidéo d\’une plateforme externe  vers Nudgis. \n \
       Remote Partiel',
       skills: [
         'Angular',
@@ -135,10 +144,10 @@ export const experiences: Record<'pro' | 'training', Experience[]> = {
       title: 'Développeur mobile stagiaire',
       organisation: 'MUUZ Excellence / Projet MVP EKTOR',
       description:
-      'Participation a l\'élaboration du MVP de l\'application mobile EKTOR: Application de prévision des ventes et des besoins RH pour restaurateurs. \n \
-      Mission : \n \
-       - Construction et architectures de certaines vues mobiles \n \
-       - Developpement d’une partie de l’API Rest \n \
+      'Participation à l\'élaboration du MVP (prototype) de l\'application mobile EKTOR: Application de prévision des ventes et des besoins RH pour restaurateurs. \
+      \n **Mission** : \
+       \n- Construction et architectures de certaines vues mobiles \n \
+       \n- Developpement d’une partie de l’API Rest \n \
        Full Remote',
       skills: ['React Native', 'Django Rest', 'Développement Mobile'],
     },
@@ -146,9 +155,9 @@ export const experiences: Record<'pro' | 'training', Experience[]> = {
       year: '2017 - 2019',
       title: 'Téléopérateur et assistant de régulation médical',
       organisation: 'SOS Médecins Paris',
-      description:'Prise d’appels et régulation médicale pour l\'envoi d\'un médecin a domicile ou redirection vers les services d\'urgence.',
+      description:'Prise d’appels et régulation médicale pour l\'envoi d\'un médecin à domicile ou redirection vers les services d\'urgence.',
       skills: [
-        "Communication d'equipe rigoureuse",
+        "Communication d'équipe",
         'Gestion de situations critiques',
         'Réactivité',
       ],
@@ -157,7 +166,7 @@ export const experiences: Record<'pro' | 'training', Experience[]> = {
   training: [
     {
       year: '2023',
-      title: 'Master Informatique - Developpement Web - Alternance',
+      title: 'Master Informatique - Développement Web - Alternance',
       organisation: 'EFREI Paris',
       skills: [
         'React',
